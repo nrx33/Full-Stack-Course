c2157d661a31664e5457e2d7290594522c7626bc
Nazmul <111246433+nrx33@users.noreply.github.com>
2024-06-24 22:59:27 +0000

    added exercise 2.3

--- part_2/courseinfo/src/components/Course.jsx
+++ part_2/courseinfo/src/components/Course.jsx
@@ -1,11 +1,12 @@
 const Course = ({course}) => {
+    const total = course.parts.reduce((sum, part) => sum + part.exercises, 0)
     return (
         <div>
             <h1>{course.name}</h1>
             {course.parts.map(part => 
                 <p key={part.id}>{part.name} {part.exercises}</p>
             )}
-            <p><b>Total of {course.parts.reduce((sum, part) => sum + part.exercises, 0)} exercises</b></p>
+            <p><b>Total of {total} exercises</b></p>
         </div>
     )
 }
